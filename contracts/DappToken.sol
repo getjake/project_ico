2b72fbb8b560f7054cf9e0f4ad9adbaf5de147c2
jake <bozi.chinese@gmail.com>
2021-07-01 10:02:08 +0800

    1. Smoke test

---
+++ contracts/DappToken.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract DappToken {
+    // Constructor
+    // Set total num of tokens
+    // Read total num of tokens
+
+    // declare variables
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
