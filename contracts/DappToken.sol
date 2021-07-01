6efc8a02c00ad3c3cf5a603b21f7fa55127d5492
jake <bozi.chinese@gmail.com>
2021-07-01 17:07:57 +0800

    add transfer func.

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -10,13 +10,15 @@ contract DappToken {
     
     mapping(address => uint256) public balanceOf;
 
-    mapping(address => uint256) public balanceOf;
-
     constructor(uint256 _initialSupply) public {
         // totalSupply = 1000000;
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
     }
 
-
+    // Transfer function
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        // Exception if account doesnt have enough bal.
+        require(balanceOf[msg.sender] >= _value);
+    }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -30,6 +30,14 @@ contract('DappToken', function(accounts){
         });
     });
 
+    it('transfers token ownership', function() {
+        return DappToken.deployed().then(function(instance) {
+            tokenInstance = instance;
+            return tokenInstance.transfer.call(accounts[1], 99999999999999999999999);
+        }).then(assert.fail).catch(function(error) {
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert.');
+        });
+    });
 
 
-})
+});
