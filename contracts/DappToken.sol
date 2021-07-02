e6a8bfcc7c617fe8c31bcad22807256814f22ea4
jake <bozi.chinese@gmail.com>
2021-07-02 08:48:00 +0800

    add transfer func.

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -8,6 +8,12 @@ contract DappToken {
     string public standard = "DApp Token v1.0";
     uint256 public totalSupply;
     
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
@@ -20,5 +26,12 @@ contract DappToken {
     function transfer(address _to, uint256 _value) public returns (bool success) {
         // Exception if account doesnt have enough bal.
         require(balanceOf[msg.sender] >= _value);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+
+        // Transfer Event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
