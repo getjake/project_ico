b9993b645396e0500455b953c9a8332230f13a05
jake <bozi.chinese@gmail.com>
2021-07-08 18:22:35 +0800

    finished class 13 - frontEnd buy tokens.

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,10 @@ App = {
     web3Provider: null,
     contracts: {},
     account: '0x0',
+    loading: false,
+    tokenPrice: 1e16,
+    tokensSold: 0,
+    tokensAvailable: 750000,
 
     init: function(){
         console.log("App initialized...");
@@ -14,7 +18,7 @@ App = {
             // web3 = new Web3(App.web3Provider);
             web3 = new Web3(ethereum);
             ethereum.enable();
-            
+
         } else {
             // Specify default instance if no web3 instance provided
             App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
@@ -37,12 +41,41 @@ App = {
                 App.contracts.DappToken.deployed().then(function(dappToken) {
                     console.log("DappToken Address:", dappToken.address);
                 });
-                
+
+                App.listenForEvents();
                 return App.render();
             })
         })
     },
+
+    // Listen for events emitted from the contract
+    listenForEvents: function() {
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            instance.Sell({},{
+                fromBlock: 0,
+                toBlock: 'latest',
+            }).watch(function(error, event) {
+                console.log("event triggered ", event);
+                App.render();
+            })
+        })
+    },
+
+
+
+
     render: function() {
+        if (App.loading) {
+            return;
+        }
+        App.loading = true;
+
+        var loader = $('#loader');
+        var content = $('#content');
+
+        loader.show();
+        content.hide();
+
         // Load account data
         web3.eth.getCoinbase(function(err, account) {
             if(err === null) {
@@ -50,6 +83,56 @@ App = {
                 App.account = account;
                 $('#accountAddress').html("Your Account:" + account);
             }
+        })
+        // Load token sale contract
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            dappTokenSaleInstance = instance;
+            return dappTokenSaleInstance.tokenPrice();
+        }).then(function(tokenPrice) {
+            console.log("token price -->", tokenPrice);
+            App.tokenPrice = tokenPrice;
+            $('.token-price').html(web3.fromWei(App.tokenPrice, 'ether').toNumber()); // --> <span class="token-price"></span>
+            return dappTokenSaleInstance.tokensSold();
+        }).then(function(tokensSold) {
+            App.tokensSold = tokensSold.toNumber();
+            
+            // Debug
+            // App.tokensSold = 20000;
+
+            $('.tokens-sold').html(App.tokensSold);
+            $('.tokens-available').html(App.tokensAvailable);
+
+            var progressPercent = 100 * (App.tokensSold / App.tokensAvailable);
+            $('#progress').css('width', progressPercent +'%');
+
+            // Load token contract
+            App.contracts.DappToken.deployed().then(function(instance) {
+                dappTokenInstance = instance;
+                return dappTokenInstance.balanceOf(App.account);
+            }).then(function(balance) {
+                $('.dapp-balance').html(balance.toNumber())
+                App.loading = false;
+                loader.hide();
+                content.show();
+            })
+        })
+    },
+
+    buyTokens: function() {
+        $('#content').hide();
+        $('#loader').show();
+        var numberOfToken = $('#numberOfTokens').val();
+        App.contracts.DappTokenSale.deployed().then(function(instance) {
+            return instance.buyTokens(numberOfToken, {
+                from: App.account, 
+                value: numberOfToken * App.tokenPrice,
+                gas: 500000 // gas limit
+            });
+        }).then(function(result) {
+            console.log("Tokens bought ...")
+            $('form').trigger('reset') // reset the number of tokens in form
+            $('#loader').hide();
+            $('#content').show();
         })
     }
 }
